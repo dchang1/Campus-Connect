d4b02e0891255e62f22dfb18d7ee64f150a36683
dchang1 <dchang2@swarthmore.edu>
2017-04-26 11:15:54 -0400

    Add files via upload

--- search.js
+++ search.js
@@ -1,30 +1,183 @@
-var facebook=["Facebook", "Engineering", "3.0", "Hawaii"];
-var google=["Google", "Computer Science", "4.0", "California"];
-var dell=["Dell", "Engineering", "2.0", "Hawaii"];
+var facebook=["Facebook", "Engineering", "3.0", "Hawaii", "Paid", " descriptions lmaoasdfasdfasdfasdfasfasdfasdfasdfasdfasdfasdfasdfasfdsafasfasdfsad", "5/1/17"];
+var google=["Google", "Computer Science", "4.0", "California", "Paid", " descriptions lmaoasdfasdfasdfasdfasdfasdfasdfasfasdfsadfasdfasdfasdfasdfasdfsad", "5/1/17"];
+var dell=["Dell", "Engineering", "2.0", "Hawaii", "Paid", " descriptions lmao", "5/1/17"];
 var internships=[facebook, google, dell];
 var links=["https://www.facebook.com/careers/jobs/a0I1200000IAGYKEA5/?q=software+engineer+internship&location=menlo-park", "https://developers.google.com/open-source/gsoc/", "http://www.dell.com/en-us/"];
 var results=[];
+var descriptions=[];
+var date=[];
 var j=0;
-
-if (localStorage.getItem("location")=="Unspecified") {
-for (i=0; i<internships.length; i++) {
-   if (localStorage.getItem("field")==internships[i][1] && localStorage.getItem("GPA")>=internships[i][2]) {
-     results.push(internships[i][0]);
-     document.write(results[j].link(links[i])+"<br>");   
-     j++;
-   }
+if (localStorage.getItem("field")=="Unspecified") {
+  for (i=0; i<internships.length; i++) {
+    if (localStorage.getItem("location")==internships[i][3] && localStorage.getItem("GPA")>=internships[i][2] && localStorage.getItem("pay")==internships[i][4]) {
+      results.push(internships[i][0]);
+      descriptions.push(internships[i][5]);
+      date.push(internships[i][6]);
+      document.write('<a href="' + links[i] + '" style="font-size:200%;float:left;" target="_blank">' + results[j] + '</a>' + '<span>' + descriptions[j] + '</span>' + '<span style="float:right;">Due ' + date[j] + '</span>' + '<hr>');   
+      j++;
+    }
+  }
 }
+else if (localStorage.getItem("GPA")=="Unspecified") {
+  for (i=0; i<internships.length; i++) {
+    if (localStorage.getItem("field")==internships[i][1] && localStorage.getItem("location")==internships[i][3] && localStorage.getItem("pay")==internships[i][4]) {
+      results.push(internships[i][0]);
+      descriptions.push(internships[i][5]);
+      date.push(internships[i][6]);
+      document.write('<a href="' + links[i] + '" style="font-size:200%;float:left;" target="_blank">' + results[j] + '</a>' + '<span>' + descriptions[j] + '</span>' + '<span style="float:right;">Due ' + date[j] + '</span>' + '<hr>');   
+      j++;
+    }
+  }
 }
-
+else if (localStorage.getItem("location")=="Unspecified") {
+  for (i=0; i<internships.length; i++) {
+    if (localStorage.getItem("field")==internships[i][1] && localStorage.getItem("GPA")>=internships[i][2] && localStorage.getItem("pay")==internships[i][4]) {
+      results.push(internships[i][0]);
+      descriptions.push(internships[i][5]);
+      date.push(internships[i][6]);
+      document.write('<a href="' + links[i] + '" style="font-size:200%;float:left;" target="_blank">' + results[j] + '</a>' + '<span>' + descriptions[j] + '</span>' + '<span style="float:right;">Due ' + date[j] + '</span>' + '<hr>');   
+      j++;
+    }
+  }
+}
+else if (localStorage.getItem("pay")=="Unspecified") {
+  for (i=0; i<internships.length; i++) {
+    if (localStorage.getItem("field")==internships[i][1] && localStorage.getItem("GPA")>=internships[i][2] && localStorage.getItem("location")==internships[i][3]) {
+      results.push(internships[i][0]);
+      descriptions.push(internships[i][5]);
+      date.push(internships[i][6]);
+      document.write('<a href="' + links[i] + '" style="font-size:200%;float:left;" target="_blank">' + results[j] + '</a>' + '<span>' + descriptions[j] + '</span>' + '<span style="float:right;">Due ' + date[j] + '</span>' + '<hr>');   
+      j++;
+    }
+  }
+}
+else if (localStorage.getItem("field")=="Unspecified" && localStorage.getItem("GPA")=="Unspecified") {
+  for (i=0; i<internships.length; i++) {
+    if (localStorage.getItem("location")==internships[i][3] && localStorage.getItem("pay")==internships[i][4]) {
+      results.push(internships[i][0]);
+      descriptions.push(internships[i][5]);
+      date.push(internships[i][6]);
+      document.write('<a href="' + links[i] + '" style="font-size:200%;float:left;" target="_blank">' + results[j] + '</a>' + '<span>' + descriptions[j] + '</span>' + '<span style="float:right;">Due ' + date[j] + '</span>' + '<hr>');   
+      j++;
+    }
+  }
+}
+else if (localStorage.getItem("field")=="Unspecified" && localStorage.getItem("location")=="Unspecified") {
+  for (i=0; i<internships.length; i++) {
+    if (localStorage.getItem("GPA")>=internships[i][2] && localStorage.getItem("pay")==internships[i][4]) {
+      results.push(internships[i][0]);
+      descriptions.push(internships[i][5]);
+      date.push(internships[i][6]);
+      document.write('<a href="' + links[i] + '" style="font-size:200%;float:left;" target="_blank">' + results[j] + '</a>' + '<span>' + descriptions[j] + '</span>' + '<span style="float:right;">Due ' + date[j] + '</span>' + '<hr>');   
+      j++;
+    }
+  }
+}
+else if (localStorage.getItem("field")=="Unspecified" && localStorage.getItem("pay")=="Unspecified") {
+  for (i=0; i<internships.length; i++) {
+    if (localStorage.getItem("GPA")>=internships[i][2] && localStorage.getItem("location")==internships[i][3]) {
+      results.push(internships[i][0]);
+      descriptions.push(internships[i][5]);
+      date.push(internships[i][6]);
+      document.write('<a href="' + links[i] + '" style="font-size:200%;float:left;" target="_blank">' + results[j] + '</a>' + '<span>' + descriptions[j] + '</span>' + '<span style="float:right;">Due ' + date[j] + '</span>' + '<hr>');   
+      j++;
+    }
+  }
+}
+else if (localStorage.getItem("GPA")=="Unspecified" && localStorage.getItem("location")=="Unspecified") {
+  for (i=0; i<internships.length; i++) {
+    if (localStorage.getItem("field")==internships[i][1] && localStorage.getItem("pay")==internships[i][4]) {
+      results.push(internships[i][0]);
+      descriptions.push(internships[i][5]);
+      date.push(internships[i][6]);
+      document.write('<a href="' + links[i] + '" style="font-size:200%;float:left;" target="_blank">' + results[j] + '</a>' + '<span>' + descriptions[j] + '</span>' + '<span style="float:right;">Due ' + date[j] + '</span>' + '<hr>');   
+      j++;
+    }
+  }
+}
+else if (localStorage.getItem("GPA")=="Unspecified" && localStorage.getItem("pay")=="Unspecified") {
+  for (i=0; i<internships.length; i++) {
+    if (localStorage.getItem("field")==internships[i][1] && localStorage.getItem("location")==internships[i][3]) {
+      results.push(internships[i][0]);
+      descriptions.push(internships[i][5]);
+      date.push(internships[i][6]);
+      document.write('<a href="' + links[i] + '" style="font-size:200%;float:left;" target="_blank">' + results[j] + '</a>' + '<span>' + descriptions[j] + '</span>' + '<span style="float:right;">Due ' + date[j] + '</span>' + '<hr>');   
+      j++;
+    }
+  }
+}
+else if (localStorage.getItem("location")=="Unspecified" && localStorage.getItem("pay")=="Unspecified") {
+  for (i=0; i<internships.length; i++) {
+    if (localStorage.getItem("field")==internships[i][1] && localStorage.getItem("GPA")>=internships[i][2]) {
+      results.push(internships[i][0]);
+      descriptions.push(internships[i][5]);
+      date.push(internships[i][6]);
+      document.write('<a href="' + links[i] + '" style="font-size:200%;float:left;" target="_blank">' + results[j] + '</a>' + '<span>' + descriptions[j] + '</span>' + '<span style="float:right;">Due ' + date[j] + '</span>' + '<hr>');   
+      j++;
+    }
+  }
+}
+else if (localStorage.getItem("field")=="Unspecified" && localStorage.getItem("GPA")=="Unspecified" && localStorage.getItem("location")=="Unspecified") {
+  for (i=0; i<internships.length; i++) {
+    if (localStorage.getItem("pay")==internships[i][4]) {
+      results.push(internships[i][0]);
+      descriptions.push(internships[i][5]);
+      date.push(internships[i][6]);
+      document.write('<a href="' + links[i] + '" style="font-size:200%;float:left;" target="_blank">' + results[j] + '</a>' + '<span>' + descriptions[j] + '</span>' + '<span style="float:right;">Due ' + date[j] + '</span>' + '<hr>');   
+      j++;
+    }
+  }
+}
+else if (localStorage.getItem("field")=="Unspecified" && localStorage.getItem("GPA")=="Unspecified" && localStorage.getItem("pay")=="Unspecified") {
+  for (i=0; i<internships.length; i++) {
+    if (localStorage.getItem("location")==internships[i][3]) {
+      results.push(internships[i][0]);
+      descriptions.push(internships[i][5]);
+      date.push(internships[i][6]);
+      document.write('<a href="' + links[i] + '" style="font-size:200%;float:left;" target="_blank">' + results[j] + '</a>' + '<span>' + descriptions[j] + '</span>' + '<span style="float:right;">Due ' + date[j] + '</span>' + '<hr>');   
+      j++;
+    }
+  }
+}
+else if (localStorage.getItem("field")=="Unspecified" && localStorage.getItem("location")=="Unspecified" && localStorage.getItem("pay")=="Unspecified") {
+  for (i=0; i<internships.length; i++) {
+    if (localStorage.getItem("GPA")>=internships[i][2]) {
+      results.push(internships[i][0]);
+      descriptions.push(internships[i][5]);
+      date.push(internships[i][6]);
+      document.write('<a href="' + links[i] + '" style="font-size:200%;float:left;" target="_blank">' + results[j] + '</a>' + '<span>' + descriptions[j] + '</span>' + '<span style="float:right;">Due ' + date[j] + '</span>' + '<hr>');   
+      j++;
+    }
+  }
+}
+else if (localStorage.getItem("GPA")=="Unspecified" && localStorage.getItem("location")=="Unspecified" && localStorage.getItem("pay")=="Unspecified") {
+  for (i=0; i<internships.length; i++) {
+    if (localStorage.getItem("field")==internships[i][1]) {
+      results.push(internships[i][0]);
+      descriptions.push(internships[i][5]);
+      date.push(internships[i][6]);
+      document.write('<a href="' + links[i] + '" style="font-size:200%;float:left;" target="_blank">' + results[j] + '</a>' + '<span>' + descriptions[j] + '</span>' + '<span style="float:right;">Due ' + date[j] + '</span>' + '<hr>');   
+      j++;
+    }
+  }
+}
+else if (localStorage.getItem("field")=="Unspecified" && localStorage.getItem("GPA")=="Unspecified" && localStorage.getItem("location")=="Unspecified" && localStorage.getItem("pay")=="Unspecified") {
+  for (i=0; i<internships.length; i++) {
+    results.push(internships[i][0]);
+    descriptions.push(internships[i][5]);
+    date.push(internships[i][6]);
+    document.write('<a href="' + links[i] + '" style="font-size:200%;float:left;" target="_blank">' + results[j] + '</a>' + '<span>' + descriptions[j] + '</span>' + '<span style="float:right;">Due ' + date[j] + '</span>' + '<hr>');   
+    j++;   
+  }
+}
 else {
-for (i=0; i<internships.length; i++) {
-   if (localStorage.getItem("field")==internships[i][1] && localStorage.getItem("GPA")<=internships[i][2] && localStorage.getItem("location")==internships[i][3]) {
-     results.push(internships[i][0]);
-     document.write(results[j].link(links[i])+"<br>");
-     j++;
+  for (i=0; i<internships.length; i++) {
+    if (localStorage.getItem("field")==internships[i][1] && localStorage.getItem("GPA")<=internships[i][2] && localStorage.getItem("location")==internships[i][3] && localStorage.getItem("pay")==internships[i][4]) {
+      results.push(internships[i][0]);
+      descriptions.push(internships[i][5]);
+      date.push(internships[i][6]);
+      document.write('<a href="' + links[i] + '" style="font-size:200%;float:left;" target="_blank">' + results[j] + '</a>' + '<span>' + descriptions[j] + '</span>' + '<span style="float:right;">Due ' + date[j] + '</span>' + '<hr>');   
+      j++;
+    }
+  }
 }
-}
-}
-
-
-results.sort();
--- update.js
+++ update.js
@@ -2,7 +2,22 @@ function update() {
 localStorage.setItem("field", document.getElementById('field').value);
 localStorage.setItem("GPA", document.getElementById('GPA').value);
 localStorage.setItem("location", document.getElementById('location').value);
+localStorage.setItem("pay", document.getElementById('pay').value);
 document.getElementById("field").autocomplete="off";
 document.getElementById("GPA").autocomplete="off";
 document.getElementById("location").autocomplete="off";
+document.getElementById("pay").autocomplete="off";
+
+if (localStorage.getItem("field")=="Enter your field...") {
+  localStorage.setItem("field", "Unspecified");
+}
+if (localStorage.getItem("GPA")=="Enter your GPA...") {
+  localStorage.setItem("GPA", "Unspecified");
+}
+if (localStorage.getItem("location")=="Enter your location...") {
+  localStorage.setItem("location", "Unspecified");
+}
+if (localStorage.getItem("pay")=="Paid or Unpaid") {
+  localStorage.setItem("pay", "Unspecified");
+}
 }
